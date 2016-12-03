f5b02742bb00a102ea5721c81613e92d875d43d4
harshiljethava <harshiljethava7@gmail.com>
2016-12-03 22:12:32 +0530

    Added new themes

--- install_theme.py
+++ install_theme.py
@@ -7,7 +7,7 @@ def main():
 		print("\n\033[1;31m======= Author : Harshil ===========\033[1;m")
 		print("\nBefore install themes please update ...\npress ENTER for apt-get update")
 		update_flag=input()
-		os.system("apt-get update")
+		os.system("sudo apt-get update")
 		'''print(''\n1) Install Ubuntu Tweak Tool
 			     2) Install Ubuntu Themes ')
 		main_opn_flag = int(input())
@@ -15,16 +15,21 @@ def main():
 		theme_flag = 0
 		while(theme_flag != 99 ):
 			print(''' 
-				Please enter your options ? 
-				1) Yosembiance
-				2) Vivacious Colors Gtk
-				3) Ambiance & Radiance Flat Colors Suite
-				4) Ambiance & Radiance Colors Theme
-				5) Arc Theme (Light & Dark Versions)
-				6) Paper
-				7) Flatabulous 
-				8) Ambiance Crunchy Themes
-				99) Exit
+	Please enter your options ? 
+	1) Yosembiance
+	2) Vivacious Colors Gtk
+	3) Ambiance & Radiance Flat Colors Suit
+	4) Ambiance & Radiance Colors Theme
+	5) Arc Theme (Light & Dark Versions)
+	6) Paper
+	7) Flatabulous 
+	8) Ambiance Crunchy Themes
+	9) Papirus Icon
+	10) Trevilla
+	11) Revival Icon
+	12) Masalla Icon
+	13) Numix-Circle Icon 
+	99) Exit
 				''')
 			theme_flag=int(input("\n\033[1;31menter>\033[1;m"))
 			
@@ -44,28 +49,59 @@ def main():
 				cmd2 = os.system("sudo apt-get update -y")
 				cmd3 = os.system("sudo apt-get install ambiance-flat-colors -y ")
 				cmd4 = os.system("sudo apt-get install radiance-flat-colors -y")
+				print("\n\033[1;36mAmbiance & Radiance Flat Colors Suit theme installed successfully...\033[1;m")
 			elif(theme_flag == 4 ):
 				cmd1 = os.system("sudo add-apt-repository ppa:ravefinity-project/ppa -y ")
 				cmd2 = os.system("sudo apt-get update -y")
 				cmd3 = os.system("sudo apt-get install ambiance-colors radiance-colors -y ")
+				print("\n\033[1;36mAmbiance & Radiance Colors Theme theme installed successfully...\033[1;m")
 			elif(theme_flag == 5 ):
 				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/themes -y ")
 				cmd2 = os.system("sudo apt-get update -y")
 				cmd3 = os.system("sudo apt-get install arc-theme -y ")
 				cmd4 = os.system("sudo apt-get install arc-icons")
+				print("\n\033[1;36mArc Theme (Light & Dark Versions) installed successfully...\033[1;m")
 			elif(theme_flag == 6 ):
 				cmd1 = os.system("sudo add-apt-repository ppa:snwh/pulp -y ")
 				cmd2 = os.system("sudo apt-get update -y")
 				cmd3 = os.system("sudo apt-get install paper-gtk-theme -y ")
 				cmd4 = os.system("sudo apt-get install paper-icon-theme -y ")
+				print("\n\033[1;36mPaper theme installed successfully...\033[1;m")
 			elif(theme_flag == 7 ):
 				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/themes -y ")
 				cmd2 = os.system("sudo apt-get update -y")
 				cmd3 = os.system("sudo apt-get install flatabulous-theme -y ")
+				print("\n\033[1;36mFlatabulous theme installed successfully...\033[1;m")
 			elif(theme_flag == 8 ):
 				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/themes -y ")
 				cmd2 = os.system("sudo apt-get update -y")
-				cmd3 = os.system("sudo apt-get install ambiance-crunchy-y ")
+				cmd3 = os.system("sudo apt-get install ambiance-crunchy -y ")
+				print("\n\033[1;36mAmbiance Crunchy Themes theme installed successfully...\033[1;m")
+			elif(theme_flag == 9 ):
+				cmd1 = os.system("sudo add-apt-repository ppa:varlesh-l/papirus-pack -y ")
+				cmd2 = os.system("sudo apt-get update -y")
+				cmd3 = os.system("sudo apt-get install papirus-gtk-icon-theme -y ")
+				print("\n\033[1;36m Papirus Icon theme installed successfully...\033[1;m")
+			elif(theme_flag == 10 ):
+				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/icons -y ")
+				cmd2 = os.system("sudo apt-get update -y")
+				cmd3 = os.system("sudo apt-get install trevilla-icons -y ")
+				print("\n\033[1;36mTrevilla theme installed successfully...\033[1;m")
+			elif(theme_flag == 11 ):
+				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/icons2 -y ")
+				cmd2 = os.system("sudo apt-get update -y")
+				cmd3 = os.system("sudo apt-get install revival-icons -y ")
+				print("\n\033[1;36mRevival Icon theme installed successfully...\033[1;m")
+			elif(theme_flag == 12 ):
+				cmd1 = os.system("sudo add-apt-repository ppa:noobslab/icons -y ")
+				cmd2 = os.system("sudo apt-get update -y")
+				cmd3 = os.system("sudo apt-get install masalla-icon-theme -y ")
+				print("\n\033[1;36mMasalla Icon theme installed successfully...\033[1;m")
+			elif(theme_flag == 13 ):
+				cmd1 = os.system("sudo add-apt-repository ppa:numix/ppa -y ")
+				cmd2 = os.system("sudo apt-get update -y")
+				cmd3 = os.system("sudo apt-get install numix-icon-theme -y ")
+				print("\n\033[1;36m Numix-Circle Icon theme installed successfully...\033[1;m")
 			else:
 				print("\nTerminating ....\
 					\nBye ...")
